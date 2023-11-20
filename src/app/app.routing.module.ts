9a21a05a0e7a27efac42d6f681ced6d14ab75bcd
Maicol bautista <pirlo1121s@gmail.com>
2023-11-20 17:43:51 -0500

    {+} auth.routing, agrega rutas login and register

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -6,14 +6,33 @@ import { StoreComponent } from './pages/store/store.component';
 import { TalleresComponent } from './pages/talleres/talleres.component';
 import { HomeComponent } from './pages/home/home.component';
 
+// const routes: Routes = [
+//   {path: "", component: HomeComponent},
+//   {path: "servicios", component: SevicesComponent},
+//   {path: "agenda-una-cita", component: AgendaComponent},
+//   {path: "tienda", component: StoreComponent},
+//   {path: "talleres", component: TalleresComponent},
+//   {path: "**", redirectTo: ""},
+// ];
+
 const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "servicios", component: SevicesComponent},
-  {path: "agenda-una-cita", component: AgendaComponent},
-  {path: "tienda", component: StoreComponent},
-  {path: "talleres", component: TalleresComponent},
-  {path: "**", redirectTo: ""},
+  /** Ruta a los componentes del Modulo Auth */
+  {
+    path: 'auth',
+    loadChildren: () => import( './auth/auth.module' ).then( module => module.AuthModule )
+  },
+  /** Ruta a los componentes del Modulo Dashboard */
+  // {
+  //   path: 'dashboard',
+  //   loadChildren: () => import( './dashboard/dashboard.module' ).then( module => module.DashboardModule )
+  // },
+  // /** Ruta para la redireccion de rutas desconocidas */
+  // {
+  //   path: '**',
+  //   redirectTo: 'auth'
+  // }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
