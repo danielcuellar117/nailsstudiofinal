5d4f327e1bf00b2cc00857d1781b8de2eeaa200a
Daniel Cuellar <jd.cuellar10@uniandes.edu.co>
2023-11-19 21:49:45 -0500

    Se copia login/register

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    category: string;
+    createdAt: string;
+    name : string;
+    price: number;
+    quantity: number;
+    updatedAt: string;
+    urlImage: string;
+    userId: string;
+    __v: number;
+    _id: string
+}
