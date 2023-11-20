7a8c7adde94057bcc4e4c305831c2fbbe804f99e
Maicol bautista <pirlo1121s@gmail.com>
2023-11-19 19:23:27 -0500

    {+} iteracion de las cards en store 2.0

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent {
+  imagenes = [
+    { url: './assets/images/piedras.jpg', titulo: 'Piedras', descripcion: 'Descripción 1' },
+    { url: './assets/images/piedras2.jpg', titulo: 'Piedras', descripcion: 'Descripción 2' },
+    { url: './assets/images/piedras3.jpg', titulo: 'Piedras', descripcion: 'Descripción 3' },
+    { url: './assets/images/piedras4.jpg', titulo: 'Piedras', descripcion: 'Descripción 1' },
+    { url: './assets/images/pinceles.jpg', titulo: 'Pinceles', descripcion: 'Descripción 2' },
+    { url: './assets/images/pinceles2.jpg', titulo: 'Pinceles', descripcion: 'Descripción 3' },
+    { url: './assets/images/pinceles3.jpg', titulo: 'Pinceles', descripcion: 'Descripción 1' },
+    { url: './assets/images/pinceles4.jpg', titulo: 'Pinceles', descripcion: 'Descripción 2' },
+    { url: './assets/images/esmalte1.jpg', titulo: 'esmaltes', descripcion: 'Descripción 3' },
+  ];
 
 }
